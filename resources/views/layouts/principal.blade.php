c397c313a7d523276e40bab857760fecbd3f0782
filiprendon <frendont2223@politecnics.barcelona>
2024-02-26 17:50:52 +0100

    Link logo

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('titulo')</title>
-    <link rel="icon" type="image/x-icon" href="{{asset('img/favicon.png')}}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
@@ -16,7 +16,9 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
         <div class="container-fluid">
-            <img src="{{ asset('img/logo.png') }}" alt="" height="50px" width="250px">
+            <a href="{{url('/')}}" class="navbar-brand">
+                <img src="{{ asset('img/logo.png') }}" alt="" height="50px" width="250px">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01"
                 aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
